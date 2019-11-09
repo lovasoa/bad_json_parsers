d286fa0d0082d2a6f90fa26c4398ea1a2389347b
Ophir LOJKINE <pere.jobs@gmail.com>
2019-11-09 18:56:45 +0100

    Avoid long CI times for good parsers
    See #29

--- utils/binary_search.sh
+++ utils/binary_search.sh
@@ -5,6 +5,19 @@
 test_prog="$1"
 min=$2
 max=$3
+
+$test_prog $min
+if [ $? != 0 ]; then
+  echo "Cannot apply a binary search. Input program failed even with an input of $min" 1>&2
+  exit 1
+fi
+
+$test_prog $max
+if [ $? = 0 ]; then
+  echo "∞"
+  exit 0
+fi
+
 while [ $((min+1)) -lt $max ]
 do
   middle=$(((min+max)/2))
