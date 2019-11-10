d7362e775dd1f1fe51257c491b692a0b93eeef30
Niels Lohmann <mail@nlohmann.me>
2019-11-10 17:53:29 +0100

    add JSON for Modern C++ to CI

--- parser_cpp/parser_cpp.cpp
+++ parser_cpp/parser_cpp.cpp
@@ -1,9 +1,10 @@
+#include <iostream>
+#include <cstdlib>
 #include "json.hpp"
 
 // for convenience
 using json = nlohmann::json;
 
 int main(void) {
-  json j;
-  std::cin >> j;
+	auto j = json::parse(std::cin);
 }
