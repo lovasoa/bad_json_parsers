fd83daed07c6ede5a59e8db659c2f413d0643449
Niels Lohmann <mail@nlohmann.me>
2019-11-10 21:56:47 +0100

    simplified program

--- parser_cpp/parser_cpp.cpp
+++ parser_cpp/parser_cpp.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
-#include <cstdlib>
 #include "json.hpp"
 
-// for convenience
-using json = nlohmann::json;
-
-int main(void) {
-	auto j = json::parse(std::cin);
+int main() {
+	auto j = nlohmann::json::parse(std::cin);
 }
