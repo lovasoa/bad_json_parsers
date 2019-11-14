2a6f7b1cc7ed969aa958b13da206cf3a99a92e15
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2019-11-14 17:30:14 +0100

    port the test_parser.py script to python 2

--- test_parser.py
+++ test_parser.py
@@ -1,16 +1,19 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+# -*- encoding: utf8 -*-
 
-import asyncio
+from __future__ import print_function, unicode_literals
+
 import logging
+import subprocess
 import sys
 
-assert sys.version_info >= (3, 5), "Requires python >= 3.5 to run"
+assert sys.version_info >= (2, 7), "Requires python >= 2.7 to run"
 
 MIN_NESTING = 1
-MAX_NESTING = 5_000_000
+MAX_NESTING = 5000000
 
 assert len(sys.argv) >= 2, "Not enough arguments"
-[_, program, *arguments] = sys.argv
+program = sys.argv[1:]
 
 NESTED_ARRAYS = b'[' * MAX_NESTING + b']' * MAX_NESTING
 
@@ -20,65 +23,67 @@ logging.basicConfig(
 log = logging.getLogger(__name__)
 
 
+def print_bytes(b, out):
+    getattr(out, 'buffer', out).write(b)
+
+
 def deep_json_array(depth):
     m = len(NESTED_ARRAYS) // 2
     return NESTED_ARRAYS[m - depth:m + depth]
 
 
-async def run_program(depth, log_output=False):
-    log.info(f"Launching {program}")
-    proc = await asyncio.create_subprocess_exec(
-        program, *arguments,
-        stdin=asyncio.subprocess.PIPE,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE)
+def run_program(depth, log_output=False):
+    log.info("Launching {}".format(program))
+    proc = subprocess.Popen(
+        args=program,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
 
-    log.info(f"Writing {depth} nested arrays")
-    proc.stdin.write(deep_json_array(depth))
-    proc.stdin.close()
-    stdout, stderr = await proc.communicate()
+    log.info("Writing {} nested arrays".format(depth))
+    stdout, stderr = proc.communicate(deep_json_array(depth))
 
-    log.info(f'Command exited with {proc.returncode}')
+    log.info('Command exited with {}'.format(proc.returncode))
     if log_output and stdout:
-        log.info(f"Program's stdout:")
-        sys.stderr.buffer.write(stdout)
+        log.info("Program's stdout:")
+        print_bytes(stdout, out=sys.stdout)
     if log_output and stderr:
-        log.info(f"Program's stderr:")
-        sys.stderr.buffer.write(stderr)
+        log.info("Program's stderr:")
+        print_bytes(stderr, out=sys.stdout)
     return proc.returncode == 0
 
 
-async def binary_search(f, start, end):
+def binary_search(f, start, end):
     while not (end == start + 1):
         middle = (start + end) // 2
-        if await f(middle):
+        if f(middle):
             start = middle
         else:
             end = middle
     return end
 
 
-async def main():
-    if not await run_program(MIN_NESTING, log_output=True):
-        print(f"Cannot apply a binary search. "
-              f"Input program failed even with an input of {MIN_NESTING}", file=sys.stderr)
+def main():
+    if not run_program(MIN_NESTING, log_output=True):
+        print("Cannot apply a binary search. "
+              "Input program failed even with an input of {}".format(MIN_NESTING),
+              file=sys.stderr)
         return 1
-    if await run_program(MAX_NESTING, log_output=True):
+    if run_program(MAX_NESTING, log_output=True):
         print("∞")
         return 0
-    min_failing_depth = await binary_search(
+    min_failing_depth = binary_search(
         run_program,
         start=MIN_NESTING,
         end=MAX_NESTING)
-    log.info("The shortest valid JSON payload that cannot be parsed by "
-             f"{program} is {min_failing_depth} levels deep "
-             f"and weighs {min_failing_depth * 2 / 1000:.1f} kB")
+    log.info(("The shortest valid JSON payload that cannot be parsed by "
+              "{} is {} levels deep "
+              "and weighs {:.1f} kB"
+              ).format(' '.join(program), min_failing_depth, min_failing_depth * 2 / 1000))
     print(min_failing_depth)
     return 0
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    status = loop.run_until_complete(main())
-    loop.close()
-    exit(status)
+    exit(main())
