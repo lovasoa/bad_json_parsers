1eb979293342b1bebc38c43e8d11b9ecd33f7fb6
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2019-11-15 10:18:02 +0100

    Add error reporting to the C parser

--- parser_c_jansson/parser_c_jansson.c
+++ parser_c_jansson/parser_c_jansson.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 
 int main(int argc, char ** argv) {
-  json_t* j_test = json_loadf(stdin, JSON_DECODE_ANY, NULL);
-  return j_test == NULL ? 1 : 0;
+  json_error_t error;
+  json_t* j_test = json_loadf(stdin, JSON_DECODE_ANY, &error);
+  if (j_test == NULL) {
+    fprintf(stderr, "error at offset %d: %s\n", error.position, error.text);
+    return 1;
+  } else {
+    return 0;
+  }
 }
